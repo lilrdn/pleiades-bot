2e3cc92cc97bfcd61e4e19211a6791276635dd45
david <dale.david@mail.ru>
2021-04-08 02:06:19 +0300

    fix the complete form handler

--- core/dm.py
+++ core/dm.py
@@ -10,11 +10,12 @@ class FFDM(dialogic.dialog_manager.FormFillingDialogManager):
         super(FFDM, self).__init__(*args, **kwargs)
         self.forms_collection = forms_collection
 
-    def handle_full_form(self, form, user_object, ctx: Context):
+    def handle_completed_form(self, form, user_object, ctx: Context):
+        document = copy.deepcopy(form)
+        document['user_id'] = ctx.user_id
+        document['timestamp'] = datetime.now()
+        print('DOCUMENT IS', document)
         if self.forms_collection:
-            document = copy.deepcopy(form)
-            document['user_id'] = ctx.user_id
-            document['timestamp'] = datetime.now()
             self.forms_collection.insert_one(document)
         return Response(
             text=self.config.finish_message,
