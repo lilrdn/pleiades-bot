70721532c3b887d6243f79da01ba6de291348a18
david <dale.david@mail.ru>
2021-04-09 12:33:50 +0300

    add a test for the first message

--- test_all.py
+++ test_all.py
@@ -1,2 +1,13 @@
-def test_nothing():
-    pass
+from dialogic.testing.testing_utils import make_context
+
+from core.dm import make_dm
+from scenarios import *  # noqa
+
+
+def test_make_dm():
+    dm = make_dm()
+    ctx = make_context(text='начать', new_session=True)
+    resp = dm.respond(ctx)
+    assert resp
+    assert 'бот' in resp.text
+    assert 'начать' in resp.text
